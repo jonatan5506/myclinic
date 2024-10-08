b9a2c42e6c00723149ac2ab1f1e268170beb4da4
Jonatan_AndradeL <120528971+jonatan5506@users.noreply.github.com>
2024-10-08 11:20:52 -0300

    config da clinica

--- frontend/src/app/pages/dashboard/formularios/FormConfigApp.tsx
+++ frontend/src/app/pages/dashboard/formularios/FormConfigApp.tsx
@@ -1,7 +1,12 @@
-
+import Grid from "@mui/material/Grid2";
+import TextField from '@mui/material/TextField';
 
 export const FormConfigApp = () => {
-    return (
-        <div>Form</div>
-    )
-}
+  return (
+    <Grid container spacing={2}>
+      <Grid size={8}>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </Grid>
+    </Grid>
+  );
+};
